48a0eeb8c9cfd9a033df536842869f6664341e5c
bulatpython <bulatpython@gmail.com>
2022-12-12 14:02:27 +0300

    job in mac

---
+++ homework_2/homework_2.py
@@ -0,0 +1,14 @@
+
+
+# Введите свое решение ниже
+from collections import defaultdict
+from collections import OrderedDict
+cafes = dict()
+cafes = defaultdict(list)
+ratings = [('Old York', 3.3), ('New Age', 4.6), ('Old Gold', 3.3), ('General Foods', 4.8),
+           ('Belissimo', 4.5), ('CakeAndCoffee', 4.2), ('CakeOClock', 4.2), ('CakeTime', 4.1),
+           ('WokToWork', 4.9), ('WokAndRice', 4.9), ('Old Wine Cellar', 3.3), ('Nice Cakes', 3.9)]
+ratings.sort(key=lambda x: (-x[1], x[0]))
+for cafe, rating in ratings:
+    cafes[cafe].append(rating)
+print(cafes)
